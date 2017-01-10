a6c032e051fed1c30443f8500508c9723903d692
Zizaco Zizuini <zizaco@gmail.com>
2017-01-10 15:11:35 -0200

    Created a bran new Message class to be the standard message exchanging unit of Upswarm.

---
+++ core/Exceptions/UnserializableMessageDataException.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Core\Exceptions;
+
+use Exception;
+
+class UnserializableMessageDataException extends Exception
+{
+    public function __construct($data = null, $code = 0, $previous = null)
+    {
+        $dataAsString = is_object($data) ? sprintf("%s object", get_class($data)) : '$data';
+
+        $message = "'$dataAsString' given is not serializable. Probably there is a Closure within the object. Message \$data MUST be serializable.";
+
+        parent::__construct($message, $code);
+    }
+}
---
+++ core/Message.php
@@ -0,0 +1,191 @@
+<?php
+
+namespace Core;
+
+use Core\Exceptions\UnserializableMessageDataException;
+use React\Promise\Deferred;
+use React\Promise\Promise;
+use Exceptions;
+use stdClass;
+
+/**
+ * The Upswarm comunication unit used with the supervisor and between services.
+ * The nature of messaging enchangement is based in this Message structure.
+ */
+final class Message
+{
+    /**
+     * Unique message identifier
+     * @var string
+     */
+    public $id;
+
+    /**
+     * Type of the $data within the Message. Name of the class or type of
+     * the primitive.
+     * @var string
+     */
+    protected $type = 'null';
+
+    /**
+     * The data wrapped within the message. The data MUST be serializable in
+     * order to be sent to other services.
+     * @var string
+     */
+    protected $data;
+
+    /**
+     * The name of the service class that has created the message. If the
+     * message expects an response, this is the destination of the response.
+     * @var string
+     */
+    public $sender;
+
+    /**
+     * The "identification" of the service that this message is addressed to.
+     * It can be the name of a service class or an id.
+     * @var string
+     */
+    public $receipt;
+
+    /**
+     * Defered that may be used to resolve an promisse if the message expects
+     * an response.
+     * @var Deferred
+     */
+    protected $deferred;
+
+    /**
+     * EventEmitter instance of the service that is currently reading the
+     * message. This attribute is cleared before sending the message. It is
+     * used to indicate whenever a Message as been responded.
+     * @var \use Evenement\EventEmitter;
+     */
+    public $eventEmitter;
+
+    /**
+     * Initialize a new Message instance.
+     *
+     * @throws UnserializableMessageDataException If $data is not serializable.
+     *
+     * @param mixed  $data    Data that will be transported by the message.
+     * @param string $receipt Message destination. null means that the supervisor is the receipt.
+     */
+    public function __construct($data, string $receipt = null)
+    {
+        $this->id = uniqid('', true);
+        $this->receipt = $receipt;
+        $this->setData($data);
+    }
+
+    /**
+     * Sets the given $data as the data that will be transported by the Message.
+     *
+     * @throws UnserializableMessageDataException If $data is not serializable.
+     *
+     * @param mixed $data Anything that is serializable.
+     *
+     * @return self
+     */
+    public function setData($data)
+    {
+        if (is_object($data)) {
+            $this->type = get_class($data);
+        } else {
+            $this->type = gettype($data);
+        }
+
+        try {
+            $this->data = serialize($data);
+        } catch (Exception $e) {
+            throw new UnserializableMessageDataException($data, 0, $e);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Gets the $data of the Message.
+     * WARNING: It will return a new instance everytime it is called. So, don't
+     * use this method inside loops. Ideally 'popData' should not be called more
+     * than once.
+     *
+     * @return mixed
+     */
+    public function getData()
+    {
+        return unserialize($this->data);
+    }
+
+    /**
+     * Gets the type of the Message data. The type of the return of 'getData'
+     * method.
+     *
+     * @return string Type of data within the Message.
+     */
+    public function getDataType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * If called, the message is marked as "waiting a response". With this, the
+     * service that is sending the request will register a callback
+     * (the deferred) that will be triggered whenever the response of the
+     * message arrives back to it.
+     *
+     * @return \React\Promise\Promise Promise that will be resolved when the response arrives or if the timeout is reached.
+     */
+    public function getPromisse(): Promise
+    {
+        $this->deferred = new Deferred();
+        return $this->deferred->promise();
+    }
+
+    /**
+     * Tells if this command expects an response. Which will be true if the
+     * getPromisse was called before.
+     *
+     * @return boolean
+     */
+    public function expectsResponse(): bool
+    {
+        return null != $this->deferred;
+    }
+
+    /**
+     * Sends a response Message back to the original $sender service.
+     *
+     * @param  Message $message Message that will be sent as a response to this message.
+     *
+     * @return void
+     */
+    public function respond(Message $message)
+    {
+        // The identification should be the same in order that the service
+        // receiving the message will know that $message is responding to the
+        // current Message.
+        $message->id = $this->id;
+        $message->receipt = $this->sender;
+
+        if ($this->eventEmitter) {
+            $this->eventEmitter->emit('respond', [$message]);
+        }
+    }
+
+    /**
+     * Tells which Fields should not be discarted when serializing a message.
+     *
+     * @return array Field that will not be serialized
+     */
+    public function __sleep()
+    {
+        return [
+            'id',
+            'command',
+            'params',
+            'sender',
+            'receipt',
+        ];
+    }
+}
